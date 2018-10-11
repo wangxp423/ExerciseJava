57e559a92dd6676323258ef0d4d667698db9e254
wangxiaopan <wangxiaopan423@gmail.com>
2018-10-11 15:39:21 +0800

    Leetcode88优化方案，增加第75题。

---
+++ src/com/xp/java/leetcode/Solution_0075.java
@@ -0,0 +1,62 @@
+package com.xp.java.leetcode;
+
+import com.xp.java.util.TestUtil;
+
+/**
+ * @类描述：Leetcode75 颜色分类  0 1 2 分别代表三种颜色
+ * @创建人：Wangxiaopan
+ * @创建时间：2018/10/11 0011 11:57
+ * @修改人：
+ * @修改时间：2018/10/11 0011 11:57
+ * @修改备注：
+ */
+public class Solution_0075 {
+    //使用计数法，因为只有三种颜色，我们可以声明一个长度为三的数组用来记录各种颜色然后在进行遍历插入赋值。
+    //这里的时间复杂度为O(2n)即为O(n)，空间复杂度为O(n)
+    public static void sortColors(int[] nums) {
+        int[] colors = new int[3];
+        for (int i = 0; i < nums.length; i++) {
+            colors[nums[i]] += 1;
+        }
+        int index = 0;
+        int color = -1;
+        for (int j = 0; j < colors.length; j++) {
+            color = j;
+            for (int k = 0; k < colors[j]; k++) {
+                nums[index++] = color;
+            }
+        }
+    }
+
+    //我们可以尝试用O(n)的时间复杂度 O(1)的空间复杂度来写
+    //思路为：用两个指针分别记录: zero代表0结尾的位置,two代表2开头的位置。
+    //那么zero-two之间的位置就是1了。这样一次遍历就可以搞定了。
+    public static void sortColors1(int[] nums) {
+        int zero = -1; //nums[0-zero] = 0
+        int two = nums.length; //nums[two - (n-1)] = 2
+        for (int i = 0; i < two; ) {//这里不写i++是因为在有些情况下i不需要++
+            if (nums[i] == 1) {
+                i++;
+            } else if (nums[i] == 2) {
+                two--;
+                int temp = nums[two];
+                nums[two] = nums[i];
+                nums[i] = temp;
+            } else {
+                //这里当i<= zero的时候是不需要进行交换的
+                zero++;
+                int temp = nums[zero];
+                nums[zero] = nums[i];
+                nums[i] = temp;
+                i++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 0, 2, 1, 1, 0};
+//        sortColors(nums);
+        sortColors1(nums);
+        TestUtil.printArray("排序 = ", nums);
+    }
+}
--- src/com/xp/java/leetcode/Solution_0088.java
+++ src/com/xp/java/leetcode/Solution_0088.java
@@ -11,10 +11,30 @@ import com.xp.java.util.TestUtil;
  * @修改备注：
  */
 public class Solution_0088 {
+    //其实对于一个有序数组 用插入排序还是很高校的。当然前提是 后面数组的最小元素大于前面数组的最大元素。
+    //这里其实用的是一个插入排序算法原理。
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
         while (m < m + n) {
             int insert = nums2[--n]; //这里用--n不好，因为是有序，先插入大的 导致后插入的都要进行比较，这里这么写纯粹为了好看。
             nums1[m++] = insert;
+            //插入排序原理
+            for (int i = m - 1; i > 0; i--) {
+                if (nums1[i] - nums1[i - 1] < 0) {
+                    int temp = nums1[i];
+                    nums1[i] = nums1[i - 1];
+                    nums1[i - 1] = temp;
+                }
+            }
+        }
+    }
+
+    //其实对于一个有序数组 用插入排序还是很高校的。当然前提是 后面数组的最小元素不要大于前面数组的最大元素。
+    public static void merge1(int[] nums1, int m, int[] nums2, int n) {
+        int index = 0;
+        while (index < n) {
+            nums1[m++] = nums2[index];
+            index++;
+            //插入排序原理
             for (int i = m - 1; i > 0; i--) {
                 if (nums1[i] - nums1[i - 1] < 0) {
                     int temp = nums1[i];
@@ -28,7 +48,8 @@ public class Solution_0088 {
     public static void main(String[] args) {
         int[] nums1 = {1, 2, 3, 0, 0, 0};
         int[] nums2 = {2, 5, 6};
-        merge(nums1, 3, nums2, 3);
+//        merge(nums1, 3, nums2, 3);
+        merge1(nums1, 3, nums2, 3);
         TestUtil.printArray("合并有序数组：", nums1);
     }
 }
