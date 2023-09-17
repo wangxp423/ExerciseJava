0017260d98392b3c7dbd7ad93881aa0f798aba43
wangxiaopan <wangxiaopan423@gmail.com>
2023-09-17 09:28:58 +0800

    1,删除Pair引用

--- src/com/xp/java/leetcode/tree/Solution_0102.java
+++ src/com/xp/java/leetcode/tree/Solution_0102.java
@@ -1,7 +1,5 @@
 package com.xp.java.leetcode.tree;
 
-import javafx.util.Pair;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -248,31 +246,5 @@ public class Solution_0102 {
             res.append("] tail");
             return res.toString();
         }
-    }
-
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        ArrayList<List<Integer>> res = new ArrayList<>();
-        if (null == root) return res;
-
-        ArrayQueue<Pair<TreeNode, Integer>> queue = new ArrayQueue<>();
-        queue.enqueue(new Pair<TreeNode, Integer>(root, 0));
-        while (!queue.isEmpty()) {
-            Pair<TreeNode, Integer> front = queue.dequeue();
-            TreeNode node = front.getKey();
-            int level = front.getValue();
-            if (level == res.size()) {
-                res.add(new ArrayList<Integer>());
-            }
-            assert level < res.size();
-
-            res.get(level).add(node.val);
-            if (node.left != null) {
-                queue.enqueue(new Pair<TreeNode, Integer>(node.left, level + 1));
-            }
-            if (node.right != null) {
-                queue.enqueue(new Pair<TreeNode, Integer>(node.right, level + 1));
-            }
-        }
-        return res;
     }
 }
